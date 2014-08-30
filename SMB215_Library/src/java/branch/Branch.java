a742d6cd2f08bdce8f70dc10c6b49a9feb8f23b5
eddyghabachisae <eddyghabachisae@gmail.com>
2014-08-30 22:07:10 +0300

    Added Library and Branch classes

---
+++ SMB215_Library/src/java/branch/Branch.java
@@ -0,0 +1,107 @@
+package branch;
+
+import java.util.Date;
+
+public class Branch {
+    
+    private int id;
+    private String name;
+    private int city;
+    private String address;
+    private String email;
+    private String phone;
+    private String fax;
+    private String mobile;
+    private boolean isActive;
+    private Date activeSince;
+    private Date deactivatedSince;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCity() {
+        return city;
+    }
+
+    public void setCity(int city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public Date getActiveSince() {
+        return activeSince;
+    }
+
+    public void setActiveSince(Date activeSince) {
+        this.activeSince = activeSince;
+    }
+
+    public Date getDeactivatedSince() {
+        return deactivatedSince;
+    }
+
+    public void setDeactivatedSince(Date deactivatedSince) {
+        this.deactivatedSince = deactivatedSince;
+    }
+    
+}
---
+++ SMB215_Library/src/java/library/Library.java
@@ -0,0 +1,45 @@
+package library;
+
+public class Library {
+    
+    private int id;
+    private String name;
+    // image  
+    private String website;
+    private int mainBranch;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public int getMainBranch() {
+        return mainBranch;
+    }
+
+    public void setMainBranch(int mainBranch) {
+        this.mainBranch = mainBranch;
+    }
+    
+    
+    
+}
