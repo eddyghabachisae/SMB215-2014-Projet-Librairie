23f6a8d7ca091952da95c3b868a3037ded6389a0
Solangesola <SolangeKouKou@hotmail.com>
2014-09-02 01:04:49 -0700

    Profession

---
+++ MVC/src/java/Profession/Profession.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Profession;
+
+public class Profession {
+    private int prfid;
+    private String prfdescription;
+    
+    
+    public int getprfid(){
+        return prfid;
+    }
+    
+    public void setprfid(int prfid){
+        this.prfid = prfid;
+    }
+    
+    public String getprfdescription(){
+        return prfdescription;
+    }
+    
+    public void setprfdescription(String prfdescription){
+        this.prfdescription = prfdescription;
+    }
+    
+}
