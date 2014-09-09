390a33c52350372d7f464c38061b7b9f7fbf9ea2
eddyghabachisae <eddyghabachisae@gmail.com>
2014-09-09 18:01:48 +0300

    library entity

--- SMB215_Library/src/java/library/Library.java
+++ SMB215_Library/src/java/library/Library.java
@@ -7,6 +7,14 @@ public class Library {
     // image  
     private String website;
     private int mainBranch;
+    private int rentalDays;
+    private int reservationDays;
+    private int maxReserve;
+    private int rentalAlert;
+    private int reservationAlert;
+    private String mainCurrency;
+    private String secondaryCurrency;
+    private float secondaryCurrencyRate;
 
     public int getId() {
         return id;
@@ -40,6 +48,69 @@ public class Library {
         this.mainBranch = mainBranch;
     }
     
-    
+        public int getRentalDays() {
+        return rentalDays;
+    }
+
+    public void setRentalDays(int rentalDays) {
+        this.rentalDays = rentalDays;
+    }
+
+    public int getReservationDays() {
+        return reservationDays;
+    }
+
+    public void setReservationDays(int reservationDays) {
+        this.reservationDays = reservationDays;
+    }
+
+    public int getMaxReserve() {
+        return maxReserve;
+    }
+
+    public void setMaxReserve(int maxReserve) {
+        this.maxReserve = maxReserve;
+    }
+
+    public int getRentalAlert() {
+        return rentalAlert;
+    }
+
+    public void setRentalAlert(int rentalAlert) {
+        this.rentalAlert = rentalAlert;
+    }
+
+    public int getReservationAlert() {
+        return reservationAlert;
+    }
+
+    public void setReservationAlert(int reservationAlert) {
+        this.reservationAlert = reservationAlert;
+    }
+
+    public String getMainCurrency() {
+        return mainCurrency;
+    }
+
+    public void setMainCurrency(String mainCurrency) {
+        this.mainCurrency = mainCurrency;
+    }
+
+    public String getSecondaryCurrency() {
+        return secondaryCurrency;
+    }
+
+    public void setSecondaryCurrency(String secondaryCurrency) {
+        this.secondaryCurrency = secondaryCurrency;
+    }
+
+    public float getSecondaryCurrencyRate() {
+        return secondaryCurrencyRate;
+    }
+
+    public void setSecondaryCurrencyRate(float secondaryCurrencyRate) {
+        this.secondaryCurrencyRate = secondaryCurrencyRate;
+    }
+
     
 }
