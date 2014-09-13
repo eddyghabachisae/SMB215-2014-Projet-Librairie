fd7609880536cbe5ec007dde121a3014b8156000
Douha <douha.almasri@gmail.com>
2014-09-13 23:26:34 +0300

    Enums class

---
+++ SMB215_Library/src/java/utils/Enums.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class Enums {
+
+ 
+    public enum Gender {
+	Select, Male, Female;
+    }
+
+    public enum MaritalStatus {
+	Select, Single, Engaged, Married, Divorsed;
+    }
+
+    public enum Currency {
+	None, LL, $, €;
+    }
+
+ 
+    
+
+}
