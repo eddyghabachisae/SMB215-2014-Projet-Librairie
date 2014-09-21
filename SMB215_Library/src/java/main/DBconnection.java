6b5ff35ec19908d2a1082eb7c247c1a73cc47c74
eddyghabachisae <eddyghabachisae@gmail.com>
2014-09-21 16:09:16 +0300

    modifications

--- SMB215_Library/src/java/main/DBconnection.java
+++ SMB215_Library/src/java/main/DBconnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBconnection {
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/Library";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/librarydb";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "ubuntu";
 
