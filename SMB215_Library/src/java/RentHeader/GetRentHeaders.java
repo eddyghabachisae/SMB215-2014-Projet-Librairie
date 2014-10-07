d63d0f6755bd0aa431264f6e6a2488c9bbfa4fa9
Douha <douha.almasri@gmail.com>
2014-10-07 20:05:19 +0300

    some updates

--- SMB215_Library/src/java/RentHeader/GetRentHeaders.java
+++ SMB215_Library/src/java/RentHeader/GetRentHeaders.java
@@ -6,14 +6,20 @@
 
 package RentHeader;
 
-import Item.*;
+import RentDetails.RentDetails;
+import RentDetails.RentDetailsBean;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import library.Library;
+import library.LibraryBean;
+import utils.Utils;
 
 /**
  *
@@ -35,11 +41,42 @@ public class GetRentHeaders extends HttpServlet {
             throws ServletException, IOException {
         RentHeaderBean rentHeaderBean = new RentHeaderBean();
         List<RentHeader> rentHeadersList = rentHeaderBean.getRentHeaders();
+        LibraryBean libraryBean = new LibraryBean();
+        Library library = libraryBean.getLibrary();
+        long rentalDays =0;
+        if(library!=null){
+         rentalDays = library.getRentalDays();
+        }
+        HashMap<RentHeader,String> rentHeaderMap = new HashMap<RentHeader,String>();
+        for(RentHeader rentHeader: rentHeadersList){
+            String msg = "";
+            RentDetailsBean bean = new RentDetailsBean();
+            List<RentDetails> rentDetailsList =  bean.getRentDetailss(rentHeader.getId());
+            Date date1 = rentHeader.getSubmitDate();
+            Date date2 = Utils.getNowDateTime();
+            long daysDifference = Utils.getDifferenceBetween(date1, date2);
+            if(daysDifference < rentalDays){
+                msg = "Not Late";
+            }
+            for(RentDetails rd: rentDetailsList){
+            if((rd.getReturnedDate()==null || rd.getReturnedDate().equals("")) && (daysDifference > rentalDays)){
+                    msg= "Late";
+            }
+            if((rd.getReturnedDate()!=null && rd.getReturnedDate().equals("")==false) && (daysDifference > rentalDays)){
+                    msg= "Not Late";
+            }
+            }
+            
+            rentHeaderMap.put(rentHeader, msg);
+            System.err.println("msggg:"+msg);
+            }
+        request.setAttribute("rentHeaderMap", rentHeaderMap);
         request.setAttribute("rentHeadersList", rentHeadersList);
         int listSize = rentHeadersList.size();
         request.getRequestDispatcher("RentHeader/manage.jsp?listSize="+listSize).forward(request, response);
    
-        }
+        
+    }
   
     
         @Override
